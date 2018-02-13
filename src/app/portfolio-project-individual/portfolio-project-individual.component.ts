9a9e8551eadc3fe1f8f678461cf95f04dd45067f
mueksh1925 <mukesh.krs2008@gmail.com>
2018-02-13 10:43:06 +0530

    Add files via upload

---
+++ src/app/portfolio-project-individual/portfolio-project-individual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-project-individual',
+  templateUrl: './portfolio-project-individual.component.html',
+  styleUrls: ['./portfolio-project-individual.component.css']
+})
+export class PortfolioProjectIndividualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
